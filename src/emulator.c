21fd8fa9068189eaf8c01e3fb7b9ae418bc9c52f
Leonidas S Barbosa <leosilva@protonmail.ch>
2017-03-11 15:14:09 -0300

    checking load rom/game file

--- src/emulator.c
+++ src/emulator.c
@@ -7,8 +7,12 @@ int main(int arc, char** argv)
 	int run = 0;
 
 	chip8_init();
-	if(!chip8_load_game(argv[1]))
-		printf("%d", chip8.memory[511]);
+	/*  check if rom was load properly */
+	if(!!chip8_load_game(argv[1])) {
+		fprintf(stderr, "Program start wasn't load\n");
+		fprintf(stderr, "Start program equal %x",
+				chip8.memory[PG_START + 1]);
+	}
 
 	/* main loop */
 	while(!run) {
